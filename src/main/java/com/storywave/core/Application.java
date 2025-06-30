d49abf3f2e303bbe8cb7be6412726f75d8ce7af3
Seung Won <tmddnjsrla99@gmail.com>
2025-06-30 13:03:21 +0900

    새로운 애플리케이션 진입점 Application.java를 추가하여 프로젝트 구조를 개선합니다.

---
+++ src/main/java/com/storywave/core/Application.java
@@ -0,0 +1,13 @@
+package com.storywave.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class Application {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
