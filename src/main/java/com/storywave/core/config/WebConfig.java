5e9f8372784ee254a947b0e23fe85f1cdda7906d
Seung Won <tmddnjsrla99@gmail.com>
2025-06-30 13:03:37 +0900

    WebFlux 애플리케이션의 전역 CORS 설정을 추가하여 모든 도메인에서의 요청을 허용합니다.

---
+++ src/main/java/com/storywave/core/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.storywave.core.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+@EnableWebFlux
+public class WebConfig implements WebFluxConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**")
+            .allowedOriginPatterns("*")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true)
+            .maxAge(3600);
+    }
+}
